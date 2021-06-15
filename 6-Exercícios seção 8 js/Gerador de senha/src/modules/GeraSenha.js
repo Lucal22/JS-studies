b72a719a74aebaa7bb06ead54e68ea382f7a59cb
Luís Carlos <77598201+Lucal22@users.noreply.github.com>
2021-06-15 14:18:09 -0300

    Gerador de CPF e gerador de senhas

---
+++ 6-Exercícios seção 8 js/Gerador de senha/src/modules/GeraSenha.js
@@ -0,0 +1,10 @@
+export default class GeraSenha{
+    cliqueBotoes(){
+        document.addEventListener('click', function(e){
+            const el=e.target;
+            if(el.classList.contains('qtd-caracteres')){
+
+            }
+    }
+        )}
+}
